25e5dd2c2f6a19085668f9799d4442b7b6f0768a
DONOKO.Anoko <46491591+donokoanoko@users.noreply.github.com>
2023-11-11 03:59:04 +0900

    Ver.の上げ忘れ

--- move_heart.js
+++ move_heart.js
@@ -7,7 +7,7 @@ const archive_message = [
 function jsLoaded() {
 	if (document.getElementById("chatframe") !== null && document.getElementById("chatframe").contentWindow.document.getElementById("reaction-control-panel") != null) {
 		//Stream now
-
+		console.log("#####LIVE#####")
 		clearInterval(jsInitCheckTimer);
 
 		const heart = document.getElementById("chatframe").contentWindow.document.getElementById("reaction-control-panel");
@@ -17,17 +17,17 @@ function jsLoaded() {
 	}
 	else if (document.getElementById("chatframe") !== null && document.getElementById("chatframe").contentWindow.document.getElementById('label-text') !== null && archive_message.includes(document.getElementById("chatframe").contentWindow.document.getElementById('label-text').textContent)){
 		//Archive
-
+		console.log("#####Archive#####")
 		clearInterval(jsInitCheckTimer);
 	}
 	else if (document.getElementById("message") !== null && document.getElementById("message").textContent === "この動画ではチャットのリプレイを利用できません。"){
 		//Archive(edit)
-
+		console.log("#####Archive(edit)#####")
 		clearInterval(jsInitCheckTimer);
 	}
 	else if (document.getElementById("info-container") !== null && document.getElementById("info-container").textContent.indexOf("配信") == -1 && document.getElementById("info-container").textContent.indexOf("配信開始") !== -1){
 		//Movie
-
+		console.log("#####Movie#####")
 		clearInterval(jsInitCheckTimer);
 	};
 };
